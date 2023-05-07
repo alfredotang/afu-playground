8a24c029d3865bb24e5026614fd6aa7a9351939b
alfredotang <asdzxcca0617@gmail.com>
2023-05-08 03:45:56 +0800

    feat: copy json and txt when build end

--- scripts/build/config.mjs
+++ scripts/build/config.mjs
@@ -2,7 +2,7 @@ import { readFileSync } from 'node:fs'
 import PATH from '../constants/path.mjs'
 import watchPlugin from './plugins/watch.mjs'
 import writeTheScripts from './plugins/write-the-scripts.mjs'
-import copyTxtIntoOutdir from './plugins/copy-txt-into-outdir.mjs'
+import copyFileIntoOutdir from './plugins/copy-file-into-outdir.mjs'
 import { getEntryPoints } from '../utils/main.mjs'
 
 const pkg = JSON.parse(readFileSync(PATH.PKG))
@@ -17,7 +17,7 @@ export default {
   platform: 'node',
   outdir: 'dist',
   target: 'node16',
-  plugins: [watchPlugin, writeTheScripts, copyTxtIntoOutdir],
+  plugins: [watchPlugin, writeTheScripts, copyFileIntoOutdir],
   minify: true,
   external, // add any external dependencies here
 }
--- scripts/build/plugins/copy-file-into-outdir.mjs
+++ scripts/build/plugins/copy-file-into-outdir.mjs
@@ -2,12 +2,12 @@ import { statSync, readdirSync, copyFileSync, mkdirSync } from 'node:fs'
 import path from 'node:path'
 import { consola } from 'consola'
 
-const copyTxtFiles = (entryPoint, outdir) => {
+const copyFiles = (entryPoint, outdir) => {
   readdirSync(entryPoint).forEach(file => {
     const filePath = path.join(entryPoint, file)
     const entryPointDir = entryPoint.replace('src/app/', '')
 
-    if (path.extname(file) === '.txt') {
+    if (path.extname(file) === '.txt' || path.extname(file) === '.json') {
       const destPath = path.join(outdir, entryPointDir, file)
       copyFileSync(filePath, destPath)
     }
@@ -15,14 +15,14 @@ const copyTxtFiles = (entryPoint, outdir) => {
     if (statSync(filePath).isDirectory()) {
       const subDir = path.join(entryPoint, file)
       mkdirSync(path.join(outdir, entryPointDir, file), { recursive: true })
-      copyTxtFiles(subDir, outdir)
+      copyFiles(subDir, outdir)
     }
   })
 }
 
 /** @type import('esbuild').Plugin*/
 export default {
-  name: 'copy-txt-into-outdir-plugin',
+  name: 'copy-file-into-outdir-plugin',
   setup(build) {
     build.onEnd(result => {
       if (result.errors.length > 0) {
@@ -31,7 +31,7 @@ export default {
       }
 
       build.initialOptions.entryPoints.forEach(entryPoint => {
-        copyTxtFiles(entryPoint.replace('/main.ts', ''), build.initialOptions.outdir)
+        copyFiles(entryPoint.replace('/main.ts', ''), build.initialOptions.outdir)
       })
 
       consola.success(`Copy .txt files completed!`)
