95a23a519abfc5f442dc6ca421ae46fa0374e992
alfredotang <asdzxcca0617@gmail.com>
2023-05-23 16:26:50 +0800

    feat: handle quit the build watch

--- scripts/build/main.mjs
+++ scripts/build/main.mjs
@@ -1,6 +1,5 @@
 import * as esbuild from 'esbuild'
 import { consola } from 'consola'
-import chalk from 'chalk'
 import FLAG from '../constants/flag.mjs'
 import config from './config.mjs'
 
@@ -10,8 +9,10 @@ const main = async () => {
   try {
     if (flag === FLAG.WATCH) {
       await context.watch()
-      await consola.prompt(`Press ${chalk.cyanBright('Enter')} to quit`, { type: 'text', initial: 'quit' })
-      await context.dispose()
+      process.on('SIGINT', () => {
+        consola.info('quit')
+        context.dispose()
+      })
     } else {
       await context.rebuild()
       await context.dispose()
