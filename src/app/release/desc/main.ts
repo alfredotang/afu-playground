30989481d2c8b5026f472822212e7adbc7981167
alfredotang <asdzxcca0617@gmail.com>
2023-08-25 17:04:06 +0800

    feat: add release note descriptions

---
+++ src/app/release/desc/main.ts
@@ -0,0 +1,27 @@
+import { CreateIkalaGitlabAPI } from '@src/services'
+import prompt from '@src/libs/prompt'
+import logger from '@src/libs/logger'
+import { GITLAB_PROJECT_ID } from '@src/constants/gitlab'
+
+const main = async () => {
+  const targetProject = await prompt('選擇專案', {
+    type: 'select',
+    options: Object.keys(GITLAB_PROJECT_ID),
+  })
+  const api = new CreateIkalaGitlabAPI(GITLAB_PROJECT_ID[targetProject as keyof typeof GITLAB_PROJECT_ID])
+  try {
+    const { data: milestoneList } = await api.getMilestones()
+    const milestoneTitle = await prompt('選擇 milestone', {
+      type: 'select',
+      options: milestoneList.map(({ title }) => title),
+    })
+
+    const { data } = await api.getMergeRequestByMilestones(milestoneTitle)
+    const desc = data.map(({ reference, title }) => `- ${reference} ${title}`).join('\n')
+    logger.log(desc)
+  } catch (error) {
+    logger.error(error)
+  }
+}
+
+main()
