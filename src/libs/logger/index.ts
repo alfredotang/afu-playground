3e6238d5fbd0522a272c0fae01bae2abbdee487a
Alfredo Tang <asdzxcca0617@gmail.com>
2024-03-08 00:30:10 +0800

    fix: utils logger type error

--- src/libs/logger/index.ts
+++ src/libs/logger/index.ts
@@ -1,13 +1,5 @@
 import { consola } from 'consola'
 
-class Logger {
-  private _instance = consola
+const logger = consola
 
-  public log = this._instance.log
-  public start = this._instance.start
-  public success = this._instance.success
-  public info = this._instance.info
-  public error = this._instance.error
-}
-
-export default new Logger()
+export default logger
